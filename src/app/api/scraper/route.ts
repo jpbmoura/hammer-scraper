c7f6019d3c4f6a1a1bbbdc156d97b9c52f844d04
jpbmoura <jopmoura49@gmail.com>
2025-02-02 00:16:56 -0300

    first-commit

---
+++ src/app/api/scraper/route.ts
@@ -0,0 +1,166 @@
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
+
+export async function GET() {
+  console.log(`⛏  Starting scrapper`);
+
+  const scrapeUrl =
+    "https://venda-imoveis.caixa.gov.br/sistema/busca-imovel.asp?sltTipoBusca=imoveis";
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+  );
+
+  console.log(`⏳  Loading page...`);
+  await page.goto(scrapeUrl, { waitUntil: "networkidle2" });
+  console.log(`✅  Page loaded!`);
+
+  console.log(`⏳  Selecting state...`);
+  await page.waitForSelector("#cmb_estado", { visible: true });
+  await page.select("#cmb_estado", "PE");
+  console.log(`✅  State selected!`);
+
+  console.log(`⏳  Selecting city...`);
+  await page.waitForFunction(() => {
+    const cidadeSelect = document.querySelector(
+      "#cmb_cidade"
+    ) as HTMLSelectElement;
+    return cidadeSelect && cidadeSelect.options.length > 1;
+  });
+  await page.select("#cmb_cidade", "5245");
+  console.log(`✅  City selected!`);
+
+  await page
+    .waitForFunction(
+      () => {
+        const btn = document.querySelector("#btn_next0") as HTMLButtonElement;
+        return btn && !btn.disabled;
+      },
+      { timeout: 60000 }
+    )
+    .catch(() => {
+      console.log("⚠️  Botão #btn_next0 não ficou ativo a tempo.");
+    });
+
+  const nextButton0 = await page.$("#btn_next0");
+
+  if (nextButton0) {
+    console.log("⏳ Botão #btn_next0 encontrado. Tentando clicar...");
+    await page.evaluate((btn) => btn.scrollIntoView(), nextButton0);
+    await new Promise((res) => setTimeout(res, 1000));
+    await nextButton0.click();
+  } else {
+    console.log("❌ Botão #btn_next0 não encontrado. Encerrando scraping.");
+    await browser.close();
+    return NextResponse.json({
+      error:
+        "Botão de continuar não encontrado. Verifique se há imóveis disponíveis.",
+    });
+  }
+  console.log(`✅  Filters selected!`);
+
+  console.log(`⏳  Waiting for items to load...`);
+  await new Promise((res) => setTimeout(res, 2000));
+
+  const nextButton1 = await page.$("#btn_next1");
+
+  if (!nextButton1) {
+    console.log(
+      "⚠️  Botão #btn_next1 não encontrado. Verificando se a página já carregou os imóveis."
+    );
+  } else {
+    console.log("✅ Botão #btn_next1 encontrado. Tentando clicar...");
+    await nextButton1.click();
+  }
+
+  console.log(`✅  Items loaded...`);
+
+  const results = [];
+  let currentPage = 1;
+  let lastPage = 1;
+
+  while (true) {
+    console.log(`⏳  Scraping page ${currentPage}...`);
+
+    await page.waitForSelector(".dadosimovel-col2", { visible: true });
+
+    const items = await page.evaluate(() => {
+      const imoveis = Array.from(
+        document.querySelectorAll(".dadosimovel-col2")
+      );
+
+      return imoveis.map((item) => {
+        return {
+          description: item.querySelector("a")?.textContent?.trim() || "",
+          sale_value:
+            item.innerHTML
+              .match(/Valor mínimo de venda:.*?<br>/i)?.[0]
+              ?.replace(/Valor mínimo de venda:|<br>|<\/?[^>]+>/g, "")
+              .trim() || "",
+          evaluation_value:
+            item.innerHTML
+              .match(/Valor de avaliação:.*?<br>/i)?.[0]
+              ?.replace(/(<br>|Valor de avaliação:)/g, "")
+              .trim() || "",
+        };
+      });
+    });
+
+    console.log(`🔍  Found ${items.length} items on page ${currentPage}`);
+
+    results.push(...items);
+    const paginationInfo = await page.evaluate(() => {
+      const pagination = document.querySelector("#paginacao");
+
+      if (!pagination) return { currentPage: 1, lastPage: 1 };
+
+      const links = Array.from(pagination.querySelectorAll("a"));
+      const pages = links
+        .map((link) => link.textContent?.trim())
+        .filter(Boolean);
+      const lastPage = parseInt(pages[pages.length - 1] || "1", 10);
+
+      const currentPage = parseInt(
+        pagination.querySelector("b")?.textContent?.trim() || "1",
+        10
+      );
+
+      return { currentPage, lastPage };
+    });
+
+    console.log("🔍  Pagination Info:", paginationInfo);
+
+    currentPage = paginationInfo.currentPage;
+    lastPage = paginationInfo.lastPage;
+
+    console.log(`🔄  Current Page: ${currentPage}, Last Page: ${lastPage}`);
+
+    if (currentPage >= lastPage) {
+      break;
+    }
+
+    console.log(`⏳  Going to next page...`);
+
+    const nextPageLink = await page.$(
+      `#paginacao a[href*="carregaListaImoveis(${currentPage + 1})"]`
+    );
+
+    if (nextPageLink) {
+      await page.evaluate((link) => link.click(), nextPageLink);
+      await page.waitForSelector(".dadosimovel-col2", { visible: true });
+    } else {
+      break;
+    }
+  }
+
+  await browser.close();
+
+  console.log(`🎉  Scraping finished!`);
+
+  return NextResponse.json({
+    total: results.length,
+    results,
+  });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import React from "react";
+
+type TableItem = {
+  description: string;
+  sale_value: string;
+  evaluation_value: string;
+};
+
+const Home = () => {
+  const [loading, setLoading] = React.useState(true);
+  const [tableItems, setTableItems] = React.useState<TableItem[]>();
+
+  const handleScrape = async () => {
+    const response = await fetch("/api/scraper");
+    const data = await response.json();
+    setTableItems(data.results);
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    handleScrape();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl font-bold text-start w-full mb-10">
+        🔨 Hammer - Leilões de Imóveis (Arcoverde)
+      </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="w-full">
+        {loading ? (
+          <div className="text-center">
+            <p>Carregando...</p>
+          </div>
+        ) : (
+          <Table className="w-full items-center justify-items-center">
+            <TableCaption>
+              Tabela com imóveis em leilão (caixa) - Arcoverde
+            </TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Descrição</TableHead>
+                <TableHead>Valor Avaliado</TableHead>
+                <TableHead>Valor Mínimo de Venda</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {tableItems?.map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell className="font-medium">
+                    {item?.description}
+                  </TableCell>
+                  <TableCell>{item?.evaluation_value}</TableCell>
+                  <TableCell>{item?.sale_value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
